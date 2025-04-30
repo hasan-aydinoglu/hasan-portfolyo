3792bd2ceffdaa64527945ed90d0fc9d14fe42c5
it trade <ittrade@Hasans-Macbook.local>
2025-04-30 11:37:01 +0300

    my react project is ready

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hasan'ın React Projesi
+
+This project was developed by Hasan using React framework. The aim of the project is to develop frontend skills.
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, I'm Hasan 👋</h1>
+        <p>My first web project developed with React</p>
+        <div className="image-wrapper">
+          <img src="/images/hasan.jpg" alt="Hasan" className="profile-img" />
+        </div>
+        <p>This page is completely mine and was created from scratch.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
